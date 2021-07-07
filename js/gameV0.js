2859f175302ca41f72de2aef65c34e2bdb583400
Belorina <ozcan_berrin@hotmail.com>
2021-07-07 14:20:47 +0200

    home page done

--- js/gameV0.js
+++ js/gameV0.js
@@ -26,6 +26,8 @@ let score = 0;
 let goodAnswerSound;
 let wrongAnswerSound;
 
+let welcomeImage, quizText, welcomeText, menuImage;
+
 let quizzString; // = '{ "questions": [ { "title": "Quel célèbre dictateur dirigea l’URSS du milieu des années 1920 à 1953 ?", "answers": ["Lenine", "Staline", "Molotov"], "goodAnswerIndex" : 1 }, {"title": "Ma deuxième question", "answers": ["Réponse 0", "Réponse 1", "Réponse 2"],"goodAnswerIndex" : 0}]}';
 let quizz; // = JSON.parse(quizzString);
 
@@ -56,9 +58,16 @@ function preload() {
     this.load.image('labelanswer', './assets/Sprites/label2.png');
     this.load.image('play', './assets/sprites/play.png');
     this.load.image('starOn', './assets/sprites/star.png');
+
     this.load.json('questions', './assets/data/questions.json');
+
     this.load.audio('goodSound', './assets/sound/good.wav')
     this.load.audio('wrongSound', './assets/sound/wrong.wav')
+
+    this.load.image('menuBackground', './assets/sprites/windows3.png');
+    this.load.image('menu', './assets/sprites/menu.png');
+
+
 }
 
 function create() {
@@ -66,16 +75,37 @@ function create() {
     backgroundImage = this.add.image(0, 0, 'background');
     backgroundImage.setOrigin(0, 0);
     backgroundImage.setScale(0.5);
+
+    // build home screen 
+    welcomeImage = this.add.image(300, 280, 'menuBackground');
+    welcomeImage.setScale(0.8);
+    quizText = this.add.text(270, 110, "QUIZZ", { fontFamily: 'Arial', fontSize: 20, color: ' #000000 ' });
+    welcomeText = this.add.text(110, 230, "Pousser sur le bouton pour commencer le quiz.", { fontFamily: 'Arial', fontSize: 18, color: ' #000000 ' });
+    menuImage = this.add.image(300, 340, 'menu').setInteractive();
+    menuImage.setScale(0.5);
+    menuImage.on('pointerdown', displayGameScreen);
+
+    welcomeImage.setVisible(true);
+    quizText.setVisible(true);
+    welcomeText.setVisible(true);
+    menuImage.setVisible(true);
+
+
+    // quizz questions and answers
     questionImage = this.add.image(300, 100, 'labelquestion');
     questionImage.setScale(0.5);
+    questionImage.setVisible(false);
     for (let i = 0; i < answerNumber; i++) {
         answerImage[i] = this.add.image(300, 220 + i * 110, 'labelanswer').setInteractive();
         answerImage[i].on('pointerdown', () => { checkAnswer(i) });
         answerImage[i].setScale(1.0);
+        answerImage[i].setVisible(false);
     }
     questionText = this.add.text(150, 80, quizz.questions[0].title, { fontFamily: 'Arial', fontSize: 18, color: ' #ffffff ' });
+    questionText.setVisible(false);
     for (let i = 0; i < answerNumber; i++) {
         answerText[i] = this.add.text(190, 210 + i * 110, quizz.questions[0].answers[i], { fontFamily: 'Arial', fontSize: 18, color: ' #000000' });
+        answerText[i].setVisible(false);
     }
 
     playButtonImage = this.add.image(300, 550, 'play').setInteractive();
@@ -87,6 +117,7 @@ function create() {
         starImage[i] = this.add.image(30 + i * 60, 600, 'starOn');
         starImage[i].setScale(0.2);
         starImage[i].alpha = 0.5;
+        starImage[i].setVisible(false);
     }
 
     goodAnswerSound = this.sound.add('goodSound');
@@ -102,7 +133,7 @@ function checkAnswer(answerIndex) {
         goodAnswerSound.play();
         alert("OK");
         starImage[currentQuestionIndex].alpha = 1;
-        score ++;
+        score++;
     }
     else {
         wrongAnswerSound.play();
@@ -126,6 +157,28 @@ function displayNextQuestion() {
     for (let i = 0; i < 3; i++) answerImage[i].setInteractive();
 
 }
+
+function displayGameScreen() {
+
+    welcomeImage.setVisible(false);
+    quizText.setVisible(false);
+    welcomeText.setVisible(false);
+    menuImage.setVisible(false);
+
+    questionImage.setVisible(true);
+    questionText.setVisible(true);
+
+    for (let i = 0; i < answerNumber; i++) {
+        answerImage[i].setVisible(true);
+        answerText[i].setVisible(true);
+    }
+    for (let i = 0; i < 10; i++) {
+        starImage[i].setVisible(true);
+    }
+
+
+}
+
 
 
 /*
